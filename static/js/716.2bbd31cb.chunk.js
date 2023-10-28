6713948143ff50cd572f56fbf4063a0bf049417a
trevorlomba <trevorlomba@gmail.com>
2023-10-28 15:43:11 -0400

    Updates

---
+++ static/js/716.2bbd31cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmusic_demo=self.webpackChunkmusic_demo||[]).push([[716],{2716:function(e,i,s){s.r(i);s(2791);var c=s(6871),a=s(184);i.default=function(e){var i=e.visibility,s=e.visible,t=(e.toggleVisible,e.logoImage),l=e.logoImage2,o=e.activeClassName;e.song,(0,c.TH)();return(0,a.jsx)("div",{className:function(e){return e.isActive?o:void 0},children:(0,a.jsx)("img",{className:"title ".concat(i),src:"".concat(s?t:l),alt:"logo"})})}}}]);
+//# sourceMappingURL=716.2bbd31cb.chunk.js.map
